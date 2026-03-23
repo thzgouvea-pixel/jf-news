b036fb61bfc065ae279dbba1b19a50c213a3ca48
thzgouvea-pixel <thzgouvea@gmail.com>
2026-03-23 13:46:12 -0300

    Create _app.jsx

---
+++ src/pages/_app.jsx
@@ -0,0 +1,21 @@
+import Script from 'next/script'
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-J5CD56E1VX"
+        strategy="afterInteractive"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-J5CD56E1VX');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
+}
