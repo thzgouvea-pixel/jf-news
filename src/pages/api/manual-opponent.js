1ad7ca50db13daa44bad469d191eaf64872f7475
thzgouvea-pixel <thzgouvea@gmail.com>
2026-04-03 13:23:16 -0300

    Create manual-opponent.js

---
+++ src/pages/api/manual-opponent.js
@@ -0,0 +1,3 @@
+curl -X POST "https://fonsecanews.com.br/api/manual-opponent?secret=SUA_SENHA" \
+  -H "Content-Type: application/json" \
+  -d '{"name":"Gabriel Diallo","ranking":36,"country":"Canada","sofascore_id":280151}'
