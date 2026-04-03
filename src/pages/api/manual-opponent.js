c36118990fa9f74f0e8245c1f75dff34311d8dbb
thzgouvea-pixel <thzgouvea@gmail.com>
2026-04-03 13:27:56 -0300

    Delete src/pages/api/manual-opponent.js

--- src/pages/api/manual-opponent.js
+++
@@ -1,3 +0,0 @@
-curl -X POST "https://fonsecanews.com.br/api/manual-opponent?secret=SUA_SENHA" \
-  -H "Content-Type: application/json" \
-  -d '{"name":"Gabriel Diallo","ranking":36,"country":"Canada","sofascore_id":280151}'
