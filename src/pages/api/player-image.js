fbee18339e9df88b8eb68cebba071426c7f93d6c
thzgouvea-pixel <thzgouvea@gmail.com>
2026-04-06 14:18:27 -0300

    Update player-image.js

--- src/pages/api/player-image.js
+++ src/pages/api/player-image.js
@@ -2,24 +2,14 @@ export default async function handler(req, res) {
   var id = req.query.id;
   if (!id) return res.status(400).send("Missing id");
 
-  var urls = [
-    {
-      url: "https://sofascore6.p.rapidapi.com/api/sofascore/v1/team/image?team_id=" + id,
-      headers: {
-        "x-rapidapi-host": "sofascore6.p.rapidapi.com",
-        "x-rapidapi-key": process.env.RAPIDAPI_KEY,
-      },
-    },
-    {
-      url: "https://a.espncdn.com/i/headshots/tennis/players/full/" + (req.query.espn || "") + ".png",
-      headers: { "User-Agent": "FonsecaNews/9.0" },
-    },
+  var sources = [
+    "https://api.sofascore.app/api/v1/team/" + id + "/image",
+    "https://api.sofascore.app/api/v1/player/" + id + "/image",
   ];
 
-  for (var i = 0; i < urls.length; i++) {
-    if (i === 1 && !req.query.espn) continue;
+  for (var i = 0; i < sources.length; i++) {
     try {
-      var r = await fetch(urls[i].url, { headers: urls[i].headers });
+      var r = await fetch(sources[i]);
       if (r.ok) {
         var buf = await r.arrayBuffer();
         res.setHeader("Content-Type", r.headers.get("content-type") || "image/png");
