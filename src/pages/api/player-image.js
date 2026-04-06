024f79dab419abfb2eee01708085063e030d87bf
thzgouvea-pixel <thzgouvea@gmail.com>
2026-04-06 14:02:22 -0300

    Create player-image.js

---
+++ src/pages/api/player-image.js
@@ -0,0 +1,25 @@
+export default async function handler(req, res) {
+  var id = req.query.id;
+  if (!id) return res.status(400).send("Missing id");
+
+  var urls = [
+    "https://api.sofascore.app/api/v1/team/" + id + "/image",
+    "https://api.sofascore.app/api/v1/player/" + id + "/image",
+  ];
+
+  for (var i = 0; i < urls.length; i++) {
+    try {
+      var r = await fetch(urls[i], {
+        headers: { "User-Agent": "FonsecaNews/9.0" },
+      });
+      if (r.ok) {
+        var buf = await r.arrayBuffer();
+        res.setHeader("Content-Type", r.headers.get("content-type") || "image/png");
+        res.setHeader("Cache-Control", "public, max-age=86400, s-maxage=86400");
+        return res.send(Buffer.from(buf));
+      }
+    } catch (e) {}
+  }
+
+  return res.status(404).send("Not found");
+}
