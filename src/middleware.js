6292ddfda34605aeca639a7937951eb898520c8b
thzgouvea-pixel <thzgouvea@gmail.com>
2026-04-09 18:37:43 -0300

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,14 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(request) {
+  const { pathname } = request.nextUrl;
+
+  if (pathname === '/') {
+    const url = request.nextUrl.clone();
+    url.pathname = '/home-matchday-v7';
+    return NextResponse.rewrite(url);
+  }
+
   return NextResponse.next();
 }
 
