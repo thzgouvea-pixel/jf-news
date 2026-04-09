c3fcc17930cee20b5450d4ab6e9c4305fd56e602
thzgouvea-pixel <thzgouvea@gmail.com>
2026-04-09 18:09:16 -0300

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -5,7 +5,7 @@ export function middleware(request) {
 
   if (pathname === '/') {
     const url = request.nextUrl.clone();
-    url.pathname = '/home-matchday-v6';
+    url.pathname = '/home-matchday-v7';
     return NextResponse.rewrite(url);
   }
 
