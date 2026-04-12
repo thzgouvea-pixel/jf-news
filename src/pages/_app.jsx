6a7723f574d0e8aec5b7ef03d5eb37fcf6ce0d42
thzgouvea-pixel <thzgouvea@gmail.com>
2026-04-12 16:33:49 -0300

    Update _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,14 @@
 import '../styles/globals.css';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }) {
+  var router = useRouter();
+  var canonical = "https://fonsecanews.com.br" + (router.asPath === "/" ? "" : router.asPath.split("?")[0]);
   return (
     <>
       <Head>
+        <link rel="canonical" href={canonical} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Source+Serif+4:wght@400;600;700;800&family=Inter:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
